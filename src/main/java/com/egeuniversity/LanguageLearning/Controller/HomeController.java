a183e64c0908b02f7ddfb94f48781c62b3841101
meyraasahin <humeyrasahinx@gmail.com>
2022-12-19 22:23:08 +0300

    homecontroller added

---
+++ src/main/java/com/egeuniversity/LanguageLearning/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.egeuniversity.LanguageLearning.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}

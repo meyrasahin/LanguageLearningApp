08da667156d51d821aefd894f29af485df2abe02
Berfin-ux <berfin2019@gmail.com>
2022-12-20 17:34:12 +0300

    half of home page

--- src/main/java/com/egeuniversity/LanguageLearning/Controller/HomeController.java
+++ src/main/java/com/egeuniversity/LanguageLearning/Controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return "home";
+        return "index";
     }
 
     @GetMapping("/login")
